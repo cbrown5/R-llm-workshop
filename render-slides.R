7ad7cee385f258637b81156e83d43bc3abaeca74
cbrown5 <c.j.brown@utas.edu.au>
2025-03-18 22:14:25 +1100

    Initial commit

---
+++ render-slides.R
@@ -0,0 +1,11 @@
+
+# Load necessary library
+library(rmarkdown)
+
+# Define the path to the .md file
+md_file <- "genAI-overview.Rmd"
+md_file <- "LLMs-with-ellmer/tool-use-with-ellmer.Rmd"
+
+Render the .md file as io slides
+render(md_file, output_format = "ioslides_presentation")
+# render(md_file, output_format = "md_document")
